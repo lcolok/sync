b8c39a4c85ce69671fef959b847337255c390d0d
lcolok <425311101@qq.com>
2019-02-07 02:45:00 +0800

    Update index.js

--- .config/index.js
+++ .config/index.js
@@ -5,10 +5,10 @@ module.exports = {
   
     build: {
       // Template for index.html
-      index: path.resolve(path.join(__dirname, 'views'), '../index.html'),  //之前是 '../dist/index.html'
+      index: path.resolve(__dirname, '../views/index.html'),  //之前是 '../dist/index.html'
   
       // Paths
-      assetsRoot: path.resolve(path.join(__dirname,'public'), '../'),  // 之前是 '../dist'
+      assetsRoot: path.resolve(__dirname, '../'),  // 之前是 '../dist'
       assetsSubDirectory: 'static',
       assetsPublicPath: './',    // 之前是 '/'
       
