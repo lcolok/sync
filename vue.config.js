e753dc368c74a627042f3690ad9622e697ee981a
lcolok(JUB) <425311101@qq.com>
2019-03-18 15:59:56 +0800

    开始尝试Vue-CLI

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  assetsDir: 'docs',
+  publicPath: undefined,
+  outputDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
